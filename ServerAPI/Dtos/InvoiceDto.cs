9b8eb744885d07b5cbfedd4fd44bb6b51150de33
VVaRRa <158055558+0VVaRRa0@users.noreply.github.com>
2025-08-18 16:47:02 +0200

    Add Entities, Dtos

---
+++ ServerAPI/Dtos/InvoiceDto.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ServerAPI.Dtos;
+
+public class InvoiceDto
+{
+    public int Id { get; set; }
+
+    [JsonIgnore]
+    public int ClientId { get; set; }
+
+    [JsonIgnore]
+    public int ServiceId { get; set; }
+
+    public ClientDto? Client { get; set; }
+
+    public ServiceDto? Service { get; set; }
+
+    public decimal Amount { get; set; }
+
+    public bool Status { get; set; }
+
+    public string? ReceiptNumber { get; set; }
+
+    public DateTime IssueDate { get; set; }
+
+    public DateTime DueDate { get; set; }
+
+    public DateTime? PaymentDate { get; set; }
+}
---
+++ ServerAPI/Entities/RkdbContext.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ServerAPI.Entities;
+
+public partial class RkdbContext : DbContext
+{
+    public RkdbContext()
+    {
+    }
+
+    public RkdbContext(DbContextOptions<RkdbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Client> Clients { get; set; }
+
+    public virtual DbSet<Invoice> Invoices { get; set; }
+
+    public virtual DbSet<Service> Services { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=RKDb;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Client>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Client__3214EC07CB4AB443");
+
+            entity.ToTable("Client");
+
+            entity.HasIndex(e => e.Login, "UQ__Client__5E55825B08D01E21").IsUnique();
+
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.FullName).HasMaxLength(200);
+            entity.Property(e => e.Login).HasMaxLength(100);
+            entity.Property(e => e.Phone).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Invoice>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Invoice__3214EC075E21A576");
+
+            entity.ToTable("Invoice");
+
+            entity.Property(e => e.Amount).HasColumnType("decimal(10, 2)");
+            entity.Property(e => e.DueDate).HasColumnType("datetime");
+            entity.Property(e => e.IssueDate).HasColumnType("datetime");
+            entity.Property(e => e.PaymentDate).HasColumnType("datetime");
+            entity.Property(e => e.ReceiptNumber).HasMaxLength(50);
+
+            entity.HasOne(d => d.Client).WithMany(p => p.Invoices)
+                .HasForeignKey(d => d.ClientId)
+                .HasConstraintName("FK__Invoice__ClientI__3C69FB99");
+
+            entity.HasOne(d => d.Service).WithMany(p => p.Invoices)
+                .HasForeignKey(d => d.ServiceId)
+                .HasConstraintName("FK__Invoice__Service__3D5E1FD2");
+        });
+
+        modelBuilder.Entity<Service>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Service__3214EC072CBDA80C");
+
+            entity.ToTable("Service");
+
+            entity.Property(e => e.Name).HasMaxLength(200);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
