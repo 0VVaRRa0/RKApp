4171808fbe7a2b1e183175afbcd1fc9fc113f6db
VVaRRa <158055558+0VVaRRa0@users.noreply.github.com>
2025-08-18 18:09:58 +0200

    Add ClientValidator

--- ServerAPI/Validators/ServiceValidator.cs
+++ ServerAPI/Validators/ServiceValidator.cs
@@ -5,12 +5,13 @@ namespace ServerAPI.Validators;
 
 public class ServiceValidator : AbstractValidator<ServiceDto>
 {
+    private const int ServiceNameMinLength = 3;
     public ServiceValidator()
     {
         RuleFor(s => s.Name)
             .NotEmpty()
             .WithMessage("Название услуги обязательно для заполнения")
-            .MinimumLength(3)
-            .WithMessage("Название услуги должно быть не меньше 3 символов");
+            .MinimumLength(ServiceNameMinLength)
+            .WithMessage($"Название услуги должно быть не меньше {ServiceNameMinLength} символов");
     }
 }
